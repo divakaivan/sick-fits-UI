7812fb03022fa2fc4bd26847f32f34d5099cc2ce
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-07-28 14:45:33 +0300

    UPDATE the navbar to show elements depending on whether a user is signed in or not

--- components/Nav.js
+++ components/Nav.js
@@ -1,16 +1,30 @@
 import Link from "next/link";
 import React from "react";
 
+import User from "./User";
 import NavStyles from "./styles/NavStyles";
+import Signout from "./Signout";
 
 const Nav = () => (
-    <NavStyles>
-        <Link href="/items"><a>Shop</a></Link>
-        <Link href="/sell"><a>Sell</a></Link>
-        <Link href="/signup"><a>Sign up</a></Link>
-        <Link href="/orders"><a>Orders</a></Link>
-        <Link href="/me"><a>Account</a></Link>
-    </NavStyles>
+    <User>
+        {({data: {me}}) => (
+            <NavStyles>
+                <Link href="/items"><a>Shop</a></Link>
+                {me && (
+                    <> {/* because we use next.js which uses babel7, this is the same as React.Fragment */}
+                        <Link href="/sell"><a>Sell</a></Link>
+                        <Link href="/orders"><a>Orders</a></Link>
+                        <Link href="/me"><a>Account</a></Link>
+                        <Signout/>
+                    </>
+                )}
+                {!me && (
+                    <Link href="/signup"><a>Sign in</a></Link>
+                )}
+            </NavStyles>
+        )}
+    </User>
+
 );
 
 export default Nav;
