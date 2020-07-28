665918187d3b3e265b7b51a7ac25ec3f2554a859
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-07-28 20:34:22 +0300

    Prompts the user to sign in if they are not and are trying to sell an item.

--- pages/sell.js
+++ pages/sell.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Link from "next/link";
 
+import PleaseSignIn from "../components/PleaseSignIn";
 import CreateItem from "../components/CreateItem";
 
 class Sell extends React.Component {
     render() {
         return (
             <div>
-                <CreateItem/>
+                <PleaseSignIn>
+                    <CreateItem/>
+                </PleaseSignIn>
             </div>
         )
     }
