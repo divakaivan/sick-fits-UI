30be1e6f53cec9513e594af07ae18d12d6d95d16
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-08-05 13:04:23 +0300

    Add the cart to the navbar and also display the amount of items in it.

--- components/Nav.js
+++ components/Nav.js
@@ -4,6 +4,9 @@ import React from "react";
 import User from "./User";
 import NavStyles from "./styles/NavStyles";
 import Signout from "./Signout";
+import {Mutation} from "react-apollo";
+import {TOGGLE_CART_MUTATION} from "./Cart";
+import CartCount from "./CartCount";
 
 const Nav = () => (
     <User>
@@ -16,6 +19,12 @@ const Nav = () => (
                         <Link href="/orders"><a>Orders</a></Link>
                         <Link href="/me"><a>Account</a></Link>
                         <Signout/>
+                        <Mutation mutation={TOGGLE_CART_MUTATION}>
+                            {toggleCart=>(
+                                <button onClick={toggleCart}>My cart <CartCount
+                                    count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)}/></button>
+                            )}
+                        </Mutation>
                     </>
                 )}
                 {!me && (
