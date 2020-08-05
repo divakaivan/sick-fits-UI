38c0329b6f8b00fd605dd97581b3e6c63d921b6a
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-08-05 13:02:49 +0300

    Add item to cart functionality.

---
+++ components/AddToCart.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {Mutation} from "react-apollo";
+import gql from "graphql-tag";
+import {CURRENT_USER_QUERY} from "./User";
+
+const ADD_TO_CART_MUTATION = gql`
+    mutation addToCart($id: ID!) {
+        addToCart(id: $id) {
+            id
+            quantity
+        }
+    }
+`;
+
+class AddToCart extends React.Component {
+
+    render() {
+        const {id} = this.props;
+
+        return(
+            <Mutation refetchQueries={[{query: CURRENT_USER_QUERY}]} variables={{id}} mutation={ADD_TO_CART_MUTATION}>
+                {(addToCart, {loading})=>(
+                    <button disabled={loading} onClick={addToCart}>Add{loading && 'ing'} to cart</button>
+                )}
+            </Mutation>
+        )
+    }
+}
+
+export default AddToCart;
